f8b14321e6e58250104f435eeb8cf40676540f41
Nelson Pérez <bilthon@gmail.com>
2022-08-21 20:33:57 -0500

    feature: introduces simple tabs

--- components/Communities.vue
+++ components/Communities.vue
@@ -1,23 +1,20 @@
 <template>
-  <div>
-    <div class="text-h3 d-flex justify-center">Communities</div>
-    <v-container>
-      <v-row>
-        <v-col
-          v-for="community in communities"
-          cols="12"
-          lg="3"
-          md="6"
-          sm="12"
-          :key="community._id"
-        >
-          <community
-            :community="community"
-          />
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="community in communities"
+        cols="12"
+        lg="3"
+        md="6"
+        sm="12"
+        :key="community._id"
+      >
+        <community
+          :community="community"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script lang="ts">
 import Vue from 'vue'
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <communities/>
+  <div>
+    <template>
+      <v-tabs v-model="tab" class="d-flex justify-center" fixed-tabs>
+        <v-tab>Communities</v-tab>
+        <v-tab>Orders</v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item>
+          <communities/>
+        </v-tab-item>
+        <v-tab-item></v-tab-item>
+      </v-tabs-items>
+    </template>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
-  layout: 'main'
+  layout: 'main',
+  data() {
+    return {
+      tab: null
+    }
+  }
 }
 </script>
