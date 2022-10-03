9d4e5e3c6d738d291d1079e75c56bc684ab6738e
Nelson Pérez <bilthon@gmail.com>
2022-10-03 12:29:58 -0500

    feature: shows a small badge with premium/discount data, fixes #4

--- components/OrderDetails.vue
+++ components/OrderDetails.vue
@@ -12,7 +12,16 @@
       >
         <v-list-item-content>
           <v-list-item-title class="d-flex justify-space-between">
-            {{ fiatAmount }} {{ order.fiat_code }}
+            <div>
+              {{ fiatAmount }} {{ order.fiat_code }}
+              <v-badge class="mx-2 my-0 py-0"
+                offset-y="5"
+                :color="getPriceMarginColor(order)"
+                :content="(order.price_margin > 0 ? '+': '') + order.price_margin +'%'"
+              >
+
+              </v-badge>
+            </div>
             <v-chip
               style="cursor: pointer"
               outlined
@@ -87,6 +96,16 @@
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
 import { Community, OrderChannel } from '~/store/communities'
+import { Order, OrderType } from '~/store/orders'
+
+// Color scale used to classify orders by attractiveness
+const Colors = {
+  STRONG_GREEN: '#57DC0B',
+  LIGHT_GREEN: '#A0DC07',
+  AMBER: '#D8C108',
+  ORANGE: '#FC5E0A',
+  STRONG_RED: '#FC0A0A'
+}
 export default Vue.extend({
   props: ['order'],
   data() {
@@ -95,6 +114,33 @@ export default Vue.extend({
     }
   },
   methods: {
+    getPriceMarginColor(order: Order) {
+      if (order.type === OrderType.BUY) {
+        if (order.price_margin > 2) {
+          return Colors.STRONG_GREEN
+        } else if (order.price_margin > 1) {
+          return Colors.LIGHT_GREEN
+        } else if (order.price_margin > 0) {
+          return Colors.AMBER
+        } else if (order.price_margin > -1) {
+          return Colors.ORANGE
+        } else {
+          return Colors.STRONG_RED
+        }
+      } else if (order.type === OrderType.SELL) {
+        if (order.price_margin > 2) {
+          return Colors.STRONG_RED
+        } else if (order.price_margin > 1) {
+          return Colors.ORANGE
+        } else if (order.price_margin > 0) {
+          return Colors.AMBER
+        } else if (order.price_margin > -1) {
+          return Colors.LIGHT_GREEN
+        } else {
+          return Colors.STRONG_GREEN
+        }
+      }
+    },
     takeOrderClicked(e: any) {
       let url = ''
       // @ts-ignore
